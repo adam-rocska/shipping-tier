bbf51a097dd05b00949399de06067fca56c4c4be
Adam Rocska <adam.rocska@adams.solutions>
2019-09-18 15:44:24 +0200

    Created a comparable interface and its related exception.

---
+++ src/php/main/AdamRocska/ShippingTier/Comparable/Exception.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace AdamRocska\ShippingTier\Comparable;
+
+
+use AdamRocska\ShippingTier\Exception as ShippingTierException;
+
+/**
+ * Represents any generic exception that can happen within a comparable
+ * implementation's logic.
+ *
+ * @package AdamRocska\ShippingTier\Equatable
+ * @version Version 1.0.0
+ * @since   Version 1.0.0
+ * @author  Adam Rocska <adam.rocska@adams.solutions>
+ */
+class Exception extends ShippingTierException
+{
+
+}
