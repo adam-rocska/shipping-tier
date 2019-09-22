40437b07f61e182a024d34d44c15d1df902ef582
Adam Rocska <adam.rocska@adams.solutions>
2019-09-22 20:47:56 +0200

    Added some documentation to the Tier object.

--- src/php/main/AdamRocska/ShippingTier/Entity/Runtime/Tier.php
+++ src/php/main/AdamRocska/ShippingTier/Entity/Runtime/Tier.php
@@ -12,20 +12,44 @@ use AdamRocska\ShippingTier\Entity\Tier as TierEntity;
 use AdamRocska\ShippingTier\Entity\Tier\Exception\NoShippingMethodBranchesBound;
 use AdamRocska\ShippingTier\Entity\Tier\Exception\NoShippingMethodBranchForCountry;
 
+/**
+ * Represents a set of shipping method branches sorted in the same tier /
+ * category.
+ *
+ * @package AdamRocska\ShippingTier\Entity\Runtime
+ * @version Version 1.0.0
+ * @since   Version 1.0.0
+ * @author  Adam Rocska <adam.rocska@adams.solutions>
+ */
 class Tier implements TierEntity, LazyShippingMethodBranchListInjection
 {
 
     /**
+     * The machine identifier of the tier.
+     *
+     * @version Version 1.0.0
+     * @since   Version 1.0.0
+     * @author  Adam Rocska <adam.rocska@adams.solutions>
      * @var string
      */
     private $identifier;
 
     /**
+     * The human comprehensible label of the tier.
+     *
+     * @version Version 1.0.0
+     * @since   Version 1.0.0
+     * @author  Adam Rocska <adam.rocska@adams.solutions>
      * @var string
      */
     private $label;
 
     /**
+     * The set of shipping method branches encapsulated by this tier.
+     *
+     * @version Version 1.0.0
+     * @since   Version 1.0.0
+     * @author  Adam Rocska <adam.rocska@adams.solutions>
      * @var ShippingMethodBranch[]
      */
     private $shippingMethodBranches;
@@ -33,8 +57,12 @@ class Tier implements TierEntity, LazyShippingMethodBranchListInjection
     /**
      * Tier constructor.
      *
-     * @param string $identifier
+     * @version Version 1.0.0
+     * @since   Version 1.0.0
+     * @author  Adam Rocska <adam.rocska@adams.solutions>
+     *
      * @param string $label
+     * @param string $identifier
      */
     public function __construct(string $identifier, string $label)
     {
@@ -44,6 +72,11 @@ class Tier implements TierEntity, LazyShippingMethodBranchListInjection
     }
 
     /**
+     * Returns the machine identifier of the tier.
+     *
+     * @version Version 1.0.0
+     * @since   Version 1.0.0
+     * @author  Adam Rocska <adam.rocska@adams.solutions>
      * @return string
      */
     public function getIdentifier(): string
@@ -52,6 +85,11 @@ class Tier implements TierEntity, LazyShippingMethodBranchListInjection
     }
 
     /**
+     * Returns the human comprehensible label of the tier.
+     *
+     * @version Version 1.0.0
+     * @since   Version 1.0.0
+     * @author  Adam Rocska <adam.rocska@adams.solutions>
      * @return string
      */
     public function getLabel(): string
@@ -60,19 +98,26 @@ class Tier implements TierEntity, LazyShippingMethodBranchListInjection
     }
 
     /**
+     * Returns the fastest shipping method branch within the tier, that
+     * contains the provided country.
+     *
+     * @version Version 1.0.0
+     * @since   Version 1.0.0
+     * @author  Adam Rocska <adam.rocska@adams.solutions>
+     *
      * @param Country $country
      *
      * @return ShippingMethodBranch
-     * @throws NoShippingMethodBranchForCountry Throws a
-     *                                          `NoShippingMethodBranchForCountry`
-     *                                          exception when there is no
-     *                                          shipping method branch found
-     *                                          for the provided country.
      * @throws NoShippingMethodBranchesBound Throws a
      *                                      `NoShippingMethodBranchesBound`
      *                                      exception when there are no
      *                                      shipping method branches bound to
      *                                      the tier at all.
+     * @throws NoShippingMethodBranchForCountry Throws a
+     *                                          `NoShippingMethodBranchForCountry`
+     *                                          exception when there is no
+     *                                          shipping method branch found
+     *                                          for the provided country.
      */
     public function getFastestShippingMethodBranchForCountry(
         Country $country
@@ -140,12 +185,16 @@ class Tier implements TierEntity, LazyShippingMethodBranchListInjection
     }
 
     /**
+     * Compares the received `DoorToDoorTransitTime` against the received
+     * "best" / fastest `DoorToDoorTransitTime`, and tells whether the new one
+     * is considered to be faster, than the current best.
+     * The decision logic finds the one, that's most likely to arrive sooner.
+     *
      * @version Version 1.0.0
      * @since   Version 1.0.0
      * @author  Adam Rocska <adam.rocska@adams.solutions>
      *
      * @param DoorToDoorTransitTime $best
-     *
      * @param DoorToDoorTransitTime $current
      *
      * @return bool
