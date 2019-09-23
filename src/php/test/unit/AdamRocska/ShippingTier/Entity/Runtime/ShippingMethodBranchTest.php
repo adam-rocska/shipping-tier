e34f7301251242bc755241b78694791a36c04186
Adam Rocska <adam.rocska@adams.solutions>
2019-09-23 16:00:36 +0200

    Fixed packaging, and copyright information to involved files.

--- src/php/test/unit/AdamRocska/ShippingTier/Entity/Runtime/ShippingMethodBranchTest.php
+++ src/php/test/unit/AdamRocska/ShippingTier/Entity/Runtime/ShippingMethodBranchTest.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Copyright 2019 Adam Rocska
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 namespace AdamRocska\ShippingTier\Entity\Runtime;
 
@@ -124,9 +133,9 @@ class ShippingMethodBranchTest extends TestCase
                 "Should return true for a country object of iso $supportedCountryCode."
             );
         }
-        $nonExistantCountry = $this->createMockCountry("DOESNT EXIST");
+        $nonExistentCountry = $this->createMockCountry("DOESNT EXIST");
         $this->assertFalse(
-            $shippingMethod->hasCountry($nonExistantCountry),
+            $shippingMethod->hasCountry($nonExistentCountry),
             "Should return false for a country object that wasn't injected."
         );
     }
